3696fd6a48f5e7524f2fb409c327919d408db56b
Hazrat Mosaddique Ali <buildwithhazrat@gmail.com>
2025-01-19 03:51:05 +0000

    last one

--- js-practice-project/tailwind.config.js
+++ js-practice-project/tailwind.config.js
@@ -1,6 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"];
 export const theme = {
-  extend: {},
+  extend: {
+    animation: {
+      gradient: "gradient 6s ease infinite",
+    },
+    keyframes: {
+      gradient: {
+        "0%, 100%": { backgroundPosition: "0% 50%" },
+        "50%": { backgroundPosition: "100% 50%" },
+      },
+    },
+  },
 };
 export const plugins = [];
